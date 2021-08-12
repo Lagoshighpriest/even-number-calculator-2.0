bedd2d94bfb9b78fda14082709cca413276fafe6
SoulMotivation <84948231+Lagoshighpriest@users.noreply.github.com>
2021-08-12 15:05:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Write your code below this row 👇
+even_number = 0
+for n in range (2, 101 , 2):
+  even_number += n
+print(even_number)
+
+even_number2 = 0
+for n in range (2, 101, 2):
+  if even_number2% 2 == 0:
+    even_number2 += n
+print(even_number2)
+
